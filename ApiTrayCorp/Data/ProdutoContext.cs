cfa6af0d57b4f6f5b85f072a1b87c1114ca59db2
luciano <lucianoliv2@hotmail.com>
2022-08-03 21:45:50 -0300

    Criar classes de acesso de dados

---
+++ ApiTrayCorp/Data/ProdutoContext.cs
@@ -0,0 +1,10 @@
+﻿using ApiTrayCorp.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiTrayCorp.Data
+{
+    public class ProdutoContext : DbContext
+    {
+        public virtual DbSet<Produtos> Produtos { get; set; }
+    }
+}
---
+++ ApiTrayCorp/Service/ProdutosService.cs
@@ -0,0 +1,39 @@
+﻿using ApiTrayCorp.Model;
+
+namespace ApiTrayCorp.Service
+{
+    public class ProdutosService
+    {
+        private ApiTrayCorp.Data.ProdutoContext _context;
+
+        public ProdutosService(ApiTrayCorp.Data.ProdutoContext produtoContext)
+        {
+            _context = produtoContext;
+        }
+
+        public List<Produtos> GetProdutoById(Guid produtoId)
+        {
+            return _context.Produtos.Where(p => p.Id == produtoId).ToList();
+        }
+
+        public List<Produtos> GetAllProdutos()
+        {
+            return _context.Produtos.Where(p => p.Id != Guid.Empty).ToList();
+        }
+
+        public void EditProduto(Produtos produto)
+        {
+             _context.Produtos.Update(produto);
+        }
+
+        public void CreateProduto(Produtos produto)
+        {
+            _context.Produtos.Add(produto);
+        }
+
+        public void DeleteProduto(Produtos produto)
+        {
+            _context.Produtos.Remove(produto);
+        }
+    }
+}
